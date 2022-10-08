2fed37cb25569e6c8437579fbfdbf6c0e8514b09
rtof <zuichuan@msn.com>
2022-10-08 16:12:08 -0300

    fixing uppercase

--- web/src/components/countPage.js
+++
@@ -1,9 +0,0 @@
-const CountPage = (action, page, data, setPage) => {
-    if (action === 'increase' && page < data.slice(-1)[0].from) {
-        setPage(page + 1);
-    } else if (action === 'decrease' && page > 1) {
-        setPage(page - 1);
-    };
-};
-
-export default CountPage;
--- web/src/components/deleteRecord.js
+++
@@ -1,9 +0,0 @@
-const DeleteRecord = async (id, name, api, getData) => {
-    if (window.confirm(`Excluir ${name}?`)) {
-        await api.delete(`customers/${id}`)
-        .then(() => getData())
-        .catch(e => console.log(e));
-    };
-};
-
-export default DeleteRecord;
