ccaa0d2a0ccb8418f976bc30ed574b2f745ac3ac
rtof <zuichuan@msn.com>
2022-10-17 09:54:35 -0300

    check email

--- web/src/pages/customer.js
+++ web/src/pages/customer.js
@@ -31,149 +31,131 @@ const Customer = () => {
 
     const insertCustomer = async () => {
       const cpf = values.cpf.replace(/[^\w\s]/gi, '');
-      if (!CheckCPF(cpf)) {
+      if (!CheckCPF(cpf))
         return alert('CPF inválido!');
-      }
 
       if (!values.name || !values.address || !values.cpf) {
-        alert('Atenção! Os campos obrigatórios devem ser preenchidos.')
+        alert('Atenção! Os campos obrigatórios devem ser preenchidos.');
       } else {
-        let checkEmail = 0;
+        const customer = { name: values.name,
+                            address: values.address,
+                            email: values.email,
+                            cpf: cpf,
+                            phone: values.phone,
+                            birth: values.birth,
+                            password: (values.password) };
 
-        await api.post('customers/checkEmail', { email: values.email })
-        .then(({ data }) => {
-              checkEmail = data[0].id
-            })
-        .catch(e => console.log(e));
-
-        if (checkEmail !== 0 && !id) {
-          alert('Email existente na base de dados');
-        } else {
-          const customer = { name: values.name,
-                             address: values.address,
-                             email: values.email,
-                             cpf: cpf,
-                             phone: values.phone,
-                             birth: values.birth,
-                             password: (values.password) };
-
-          if (id) { 
-            if (checkEmail !== 0 && checkEmail !== parseInt(id)) {
-             alert('Email cadastrado em outro registro');
-            } else {
-              await api.patch(`customers/${id}`, customer)
-                .then(() => navigate('/listCust'))
-                .catch(e => console.log(e));
-            }
-          } else {
-            await api.post('customers', customer)
-              .then(() => navigate('/listCust'))
-              .catch(e => console.log(e));
-          };
-        };
+        const query = id ? api.patch(`customers/${id}`, customer) : api.post('customers', customer);
+        console.log(query);
+        await query
+          .then(() => navigate('/listCust'))
+          .catch(e => {
+            if (e.response.status === 419)
+              alert('Email existente na base de dados!');
+          });
       };
     };
     
-      const handleClickShowPassword = () => {
-        setValues({
-          ...values,
-          showPassword: !values.showPassword,
-        });
-      };
+    const handleClickShowPassword = () => {
+      setValues({
+        ...values,
+        showPassword: !values.showPassword,
+      });
+    };
     
-      const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-      };
+    const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+    };
 
-      const getCustomer = async () => {
-        if (id) {
-          await api.get(`customers/${id}`)
-            .then(({ data }) => {
-              setValues({ name: data.name,
-                          address: data.address,
-                          email: data.email,
-                          cpf: data.cpf,
-                          phone: data.phone,
-                          birth: new Date(data.birth).toISOString().split('T')[0],
-                          password: data.password });
-            })
-            .catch(e => console.log(e));
-        };
+    const getCustomer = async () => {
+      if (id) {
+        await api.get(`customers/${id}`)
+          .then(({ data }) => {
+            setValues({ name: data.name,
+                        address: data.address,
+                        email: data.email,
+                        cpf: data.cpf,
+                        phone: data.phone,
+                        birth: new Date(data.birth).toISOString().split('T')[0],
+                        password: data.password });
+          })
+          .catch(e => console.log(e));
       };
+    };
 
-      useEffect(() => {
-        getCustomer();
-      }, []);
+    useEffect(() => {
+      getCustomer();
+    }, []);
     
-      return (
-        <>
-        <h3>Cadastro de Clientes</h3>
+    return (
+      <>
+      <h3>Cadastro de Clientes</h3>
 
-        <div className="gridCustomer">
+      <div className="gridCustomer">
 
-        <Grid gap={3}
-              container
-              direction="column"
-              justifyContent="space-evenly"
-              alignItems="stretch"
-              className="gridCustomer">
+      <Grid gap={3}
+            container
+            direction="column"
+            justifyContent="space-evenly"
+            alignItems="stretch"
+            className="gridCustomer">
 
-            { id && <TextField id="txtId" label="Id" variant="outlined" value={id} disabled /> }
-            <TextField required id="txtName" label="Nome" variant="outlined" value={values.name} onChange={e => setValues({...values, name: e.target.value})} />
-            <TextField id="txtAddress" label="Endereço" variant="outlined" value={values.address} onChange={e => setValues({...values, address: e.target.value})} />
-            <TextField required id="txtEmail" label="E-mail" variant="outlined" value={values.email} onChange={e => setValues({...values, email: e.target.value})} disabled={id} />            
+          { id && <TextField id="txtId" label="Id" variant="outlined" value={id} disabled /> }
+          <TextField required id="txtName" label="Nome" variant="outlined" value={values.name} onChange={e => setValues({...values, name: e.target.value})} />
+          <TextField id="txtAddress" label="Endereço" variant="outlined" value={values.address} onChange={e => setValues({...values, address: e.target.value})} />
+          <TextField required id="txtEmail" label="E-mail" variant="outlined" value={values.email} onChange={e => setValues({...values, email: e.target.value})} disabled={id} />            
 
-            <InputMask value={values.cpf} onChange={e => setValues({...values, cpf: e.target.value})} mask="999.999.999-99" maskChar=" ">
-              {() => <TextField required id="txtCPF" label="CPF" variant="outlined" />}
-            </InputMask>
+          <InputMask value={values.cpf} onChange={e => setValues({...values, cpf: e.target.value})} mask="999.999.999-99" maskChar=" ">
+            {() => <TextField required id="txtCPF" label="CPF" variant="outlined" />}
+          </InputMask>
 
-            <InputMask value={values.phone} onChange={e => setValues({...values, phone: e.target.value})} mask="(99) 99999-9999" maskChar=" ">
-              {() => <TextField required id="txtPhone" label="Telefone" variant="outlined" />}
-            </InputMask>
+          <InputMask value={values.phone} onChange={e => setValues({...values, phone: e.target.value})} mask="(99) 99999-9999" maskChar=" ">
+            {() => <TextField required id="txtPhone" label="Telefone" variant="outlined" />}
+          </InputMask>
 
-            <TextField type="date" required id="txtBirth" label="Nascimento" variant="outlined" value={values.birth} onChange={e => setValues({...values, birth: e.target.value})} />
+          <TextField type="date" required id="txtBirth" label="Nascimento" variant="outlined" value={values.birth} onChange={e => setValues({...values, birth: e.target.value})} />
 
-            <FormControl required variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
-              <OutlinedInput
-                id="txtPassword"
-                type={values.showPassword ? 'text' : 'password'}
-                value={values.password}
-                onChange={e => setValues({...values, password: e.target.value})}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-        </FormControl>
-        
-        </Grid>
+          <FormControl required variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
+            <OutlinedInput
+              id="txtPassword"
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={e => setValues({...values, password: e.target.value})}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+      </FormControl>
+      
+      </Grid>
 
-        <Grid gap={3}
-              container
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
-              className="gridButton">
+      <Grid gap={3}
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            className="gridButton">
 
-          <Button onClick={() => insertCustomer()} variant="contained">Salvar</Button>
+        <Button onClick={() => insertCustomer()} variant="contained">Salvar</Button>
 
-          <Link to={'/'}>
-            <Button variant="contained">Cancelar</Button>
-          </Link>
-        </Grid>
-        </div>
-        </>
-      );
+        <Link to={'/'}>
+          <Button variant="contained">Cancelar</Button>
+        </Link>
+      </Grid>
+      </div>
+      </>
+    );
 };
 
 export default Customer;
